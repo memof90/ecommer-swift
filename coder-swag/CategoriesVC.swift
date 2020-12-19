1d3f590a9b5be0164dae78fc77bbe0d6814b3fd4
memo figueredo <memof80@gmail.com>
2020-12-19 17:35:11 -0500

    Initial setup

---
+++ coder-swag/CategoriesVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Memo Figueredo on 19/12/20.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
