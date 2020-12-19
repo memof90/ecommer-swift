54e5eb32995f595c2dd541256186f0c303833ca8
memo figueredo <memof80@gmail.com>
2020-12-19 18:22:05 -0500

    add UitableView adn table viewCell uIvIEW Front-end

--- coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/Controller/CategoriesVC.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class CategoriesVC: UIViewController {
+    
+//    MARK: IBOutlet
+    @IBOutlet weak var categoryTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+//MARK: FUNCTION
 
-
+    //    MARK: IBAction
 }
 
