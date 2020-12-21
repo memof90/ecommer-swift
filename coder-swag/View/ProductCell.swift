d3cfabde1c2c238f8d46a06cb4a3d40f441a61a1
memo figueredo <memof80@gmail.com>
2020-12-21 17:54:48 -0500

    add collection views to view controller

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Memo Figueredo on 21/12/20.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
